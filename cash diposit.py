5c338d339a38faa443ebb613640f62c6e1505069
Thameesan <selvanayagamthameesan@gmail.com>
2025-08-18 16:40:01 +0530

    list basics

--- cash diposit.py
+++ cash diposit.py
@@ -6,3 +6,28 @@
 5 for year 15
 5 or above
 '''
+def intrest(cash,duration,month_Or_year):
+    intrest_3_month=0.12
+    intrest_6_month=0.125
+    intrest_1_year=0.13
+    intrest_3_year=0.14
+    intrest_5_year=0.15
+    intrest_5_year_over=0.15
+    if month_Or_year=="month":
+        if duration==3:
+            cash=cash+cash*intrest_3_month*(3/12)
+        elif duration==6:
+            cash=cash+cash*intrest_6_month*(6/12)
+     if month_Or_year=="year":
+        if duration==1:
+            cash=cash+cash*intrest_1_year
+        elif duration==3:
+            cash=cash+cash*intrest_3_year
+        elif duration==5:
+            cash=cash+cash*intrest_5_year
+        elif duration>5:
+            cash=cash+cash*intrest_5_year_over
+            
+    print(cash)
+
+intrest(100000,1,'year')
--- validetion.py
+++ validetion.py
@@ -1,5 +1,5 @@
 num=input("please enter your number ")
 while not(num.isdigit()):
-    print('your num is not valid please enter your number')
-    num=input("please enter your number")
+    print('your num is not valid please enter your number ')
+    num=input()
 print('ty')
